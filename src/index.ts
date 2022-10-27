ed41e20eade5ad7e89ee681aa75b6f687673b0ba
Tiago Silva <tiago.silva.3p@outlook.com>
2022-10-27 17:20:22 -0300

    criado routes e PessoaController

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
+import routes from "./routes";
 
 AppDataSource.initialize().then(() => {
     const app = express();
 
     app.use(express.json());
 
-    app.get('/', (req, res) => {
-        return res.json('Bem vindo a API do Stetic-CLI');
-    })
+    app.use(routes);
 
     return app.listen(process.env.PORT,() => console.log(`Servidor online na porta ${process.env.PORT}`))
 })
---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.post('/pessoas');
+
+export default routes;
