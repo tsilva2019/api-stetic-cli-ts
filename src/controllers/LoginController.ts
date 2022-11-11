adc3d396ea65cb0ff6dd84543eb97c7fc336de0d
Tiago Nascimento da Silva <tsilva2004@gmail.com>
2022-11-10 22:59:59 -0300

    alterado middleware de autenticação e controller de login

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -1,53 +1,67 @@
 import { Request, Response } from "express";
-import { NotFoundError } from "../helpers/api-errors";
+import { BadRequestError, NotFoundError } from "../helpers/api-errors";
 import { pessoaRepository } from "../repositories/pessoaRepository";
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-
-
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export class LoginController {
-    
-    //Executa o login e retorna o token
-    async login(req: Request, res: Response) {
-        const { login, senha } = req.body;
-        //Verifica se o usuario existe no banco de dados
-        const user = await pessoaRepository.findOneBy([{ email: login}, { cpf: login }]);
-        if (!user) {
-            throw new NotFoundError('O login ou senha informado é inválido!');
-        } else {
-            //Se existir compara transforma a senha informada em hash e compara com a do banco dedados
-            const verifyPass = await bcrypt.compare(senha, user.senha);
-            if(!verifyPass){
-                throw new NotFoundError('O login ou senha informado é inválido!');
-            }else{
-                //Se o hash da senha for igual a do banco, então gera o token
-                const token = jwt.sign({ 
-                                        id: user.id, nome: user.nome, email: user.email
-                                        }, process.env.JWT_PASS ?? '', { expiresIn: '1d' });
-                //Retira informação desnecessária do usuario e retorna token,id,nome e e-mail
-                const {
-                    senha, 
-                    cpf,
-                    ativo,
-                    termo,
-                    anamnese,
-                    createdDate,
-                    updatedDate,
-                    deletedDate, 
-                    ...userLogin
-                } = user;   
-                
-                return res.json({
-                    user: userLogin,
-                    token: token
-                })   
-            }
-        }
-        
-    }
+  //Executa o login e retorna o token
+  async login(req: Request, res: Response) {
+    const { login, senha } = req.body;
+    //Verifica se o usuario existe no banco de dados
+    const user = await pessoaRepository.findOneBy([
+      { email: login },
+      { cpf: login },
+    ]);
+    if (!user) {
+      throw new NotFoundError("O login ou senha informado é inválido!");
+    } else {
+      //Se existir compara transforma a senha informada em hash e compara com a do banco dedados
+      const verifyPass = await bcrypt.compare(senha, user.senha);
+      if (!verifyPass) {
+        throw new NotFoundError("O login ou senha informado é inválido!");
+      } else {
+        //Se o hash da senha for igual a do banco, então gera o token
+        const token = jwt.sign(
+          {
+            id: user.id,
+            nome: user.nome,
+            email: user.email,
+          },
+          process.env.JWT_PASS ?? "",
+          { expiresIn: "1d" }
+        );
+        //Retira informação desnecessária do usuario e retorna token,id,nome e e-mail
+        const {
+          senha,
+          cpf,
+          ativo,
+          termo,
+          anamnese,
+          createdDate,
+          updatedDate,
+          deletedDate,
+          ...userLogin
+        } = user;
 
-    async getProfile(req: Request, res: Response) {
-        return res.json(['Usuario Autenticado!', req.user])
+        return res.json({
+          user: userLogin,
+          token: token,
+        });
+      }
     }
+  }
+
+  async getProfile(req: Request, res: Response) {
+    const id = req.user.id;
+    const userVerify = await pessoaRepository.findOneBy({ id });
+    //console.log(usVerify?.senha)
+    if(!userVerify) {
+      throw new NotFoundError('Profile não encontrado!');
+    }
+    const { senha, ...userProfile } = userVerify
+
+    return res.status(200).json(userProfile);
+  }
+
 }
--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,34 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import { NotFoundError, UnauthorizedError } from "../helpers/api-errors";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 import { pessoaRepository } from "../repositories/pessoaRepository";
 
 type JWTPayload = {
-    id: string
-}
+  id: string;
+};
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
-    console.log(authorization);
-    if (!authorization) {
-        throw new UnauthorizedError("Necessário token para autenticação!");
-    } else {
-        const token = authorization.split(' ')[1];
-        const user = jwt.verify(token, process.env.JWT_PASS ?? '') as JWTPayload;
-        console.log(user);
-        if (!user) {
-            throw new UnauthorizedError("O token é inválido!");
-        } else {
-            const userVerify = await pessoaRepository.findOneBy({ id: user.id });
-            console.log(userVerify)
-            if (!userVerify) {
-                throw new NotFoundError('Usuario não autorizado!');
-            } else {
-                req.user = user;
-                next();
-            }
-        }
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
 
-       
+  if (!authorization) {
+    throw new UnauthorizedError("Necessário token para autenticação!");
+  }
+  const token = authorization.split(" ")[1];
+
+  jwt.verify(token, process.env.JWT_PASS ?? "", function (err, decoded) {
+    if (err) {
+      throw new UnauthorizedError(err.message);
     }
-}
+    const user = decoded as JWTPayload;
+    if (!user) {
+      throw new UnauthorizedError("Usuário não autorizado!");
+    }
+    // const userVerify = pessoaRepository.findOneBy({
+    //   id: user.id,
+    // });
+    // if (!userVerify) {
+    //   throw new UnauthorizedError("Usuário não autorizado!");
+    // }
+    req.user = user;
+    next();
+  });
+};
